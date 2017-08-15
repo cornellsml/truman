ddb1eaef055859bb434949e5d61706e7c7deadc8
Dominic Difranzo <dominic@difranzo.com>
2017-08-14 23:15:03 -0400

    fix and added new fields to data-export

--- data-export.js
+++ data-export.js
@@ -71,6 +71,11 @@ User.find()
         mlm.id = users[i].mturkID;
         sur.id = users[i].mturkID;
 
+        mlm.email = users[i].email;
+        sur.email = users[i].email;
+
+        console.log("In User "+ users[i].email);
+
         //UI
         if (users[i].ui == 'ui')
         {
@@ -100,6 +105,86 @@ User.find()
           sur.HighBystanders = 0;
         }
 
+        if (users[i].profile.name)
+        {
+          mlm.ProfileName = 1;
+          sur.ProfileName = 1;
+        }
+        else
+        {
+          mlm.ProfileName = 0;
+          sur.ProfileName = 0;
+        }
+
+        if (users[i].profile.location)
+        {
+          mlm.ProfileLocation = 1;
+          sur.ProfileLocation = 1;
+        }
+        else
+        {
+          mlm.ProfileLocation = 0;
+          sur.ProfileLocation = 0;
+        }
+
+        if (users[i].profile.bio)
+        {
+          mlm.ProfileBio = 1;
+          sur.ProfileBio = 1;
+        }
+        else
+        {
+          mlm.ProfileBio = 0;
+          sur.ProfileBio = 0;
+        }
+
+        if (users[i].profile.picture)
+        {
+          mlm.ProfilePicture = 1;
+          sur.ProfilePicture = 1;
+        }
+        else
+        {
+          mlm.ProfilePicture = 0;
+          sur.ProfilePicture = 0;
+        }
+
+        mlm.UserAgent = users[i].log[0].userAgent;
+        sur.UserAgent = users[i].log[0].userAgent;
+
+        mlm.notificationpage = 0;
+        mlm.numberbullypage = 0;
+        mlm.numbervictimpage = 0;
+        mlm.generalpagevisit = 0;
+        for(var z = 0; z < users[i].pageLog.length; ++z){
+            if(users[i].pageLog[z].page == "Notifications")
+              mlm.notificationpage++;
+            else if (users[i].pageLog[z].page == bully_name)
+              mlm.notificationpage++;
+            else if (users[i].pageLog[z].page == "casssssssssie")
+              mlm.numbervictimpage++;
+            else
+              mlm.generalpagevisit++;
+        }
+        sur.notificationpage = mlm.notificationpage;
+        sur.numberbullypage = mlm.numberbullypage;
+        sur.numbervictimpage = mlm.numbervictimpage;
+        sur.generalpagevisit = mlm.generalpagevisit;
+
+        mlm.citevisits = users[i].log.length;
+        sur.citevisits = users[i].log.length;
+
+        if (users[i].completed)
+        {
+          mlm.CompletedStudy = 1;
+          sur.CompletedStudy = 1;
+        }
+        else
+        {
+          mlm.CompletedStudy = 0;
+          sur.CompletedStudy = 0;
+        }
+
         //per feedAction
         mlm.VictimNoBullyReplies = 0;
         mlm.VictimNoBullyLikes = 0;
@@ -109,56 +194,92 @@ User.find()
         mlm.GeneralFlagNumber = 0;
         mlm.AveReadTime = 0;
         mlm.TotalNumberRead = 0;
+        mlm.TotalNonBullyPostRead = 0;
+        var bullyLikes = 0;
+        var bullyReplies = 0;
+        var bullyReads = 0;
+        var bullyReadTimes = 0;
+        var bullyFlag = 0;
         //per feedAction
-        console.log("In User "+ users[i].email);
+  
         for (var k = users[i].feedAction.length - 1; k >= 0; k--) 
         {
-          //Victim stats
-          if (users[i].feedAction[k].post.actor.id == victim)
+          //is a bully message
+          if(users[i].feedAction[k].post.id == bully_messages[0] || users[i].feedAction[k].post.id == bully_messages[1] || users[i].feedAction[k].post.id == bully_messages[2]||users[i].feedAction[k].post.id == bully_messages[3])
           {
             if(users[i].feedAction[k].replyTime[0])
             {
-              mlm.VictimNoBullyReplies++;
+              bullyReplies++;
             }
 
             if(users[i].feedAction[k].liked)
             {
-              mlm.VictimNoBullyLikes++;
+              bullyLikes++;
             }
+
+            if(users[i].feedAction[k].flagTime[0])
+            {
+              bullyFlag++;
+            }
+
+            if(users[i].feedAction[k].readTime[0])
+            {
+              bullyReads++;
+              bullyReadTimes += users[i].feedAction[k].readTime.sum() / users[i].feedAction[k].readTime.length; 
+            }
+
           }
 
-          //bully stats
-          if (users[i].feedAction[k].post.actor.id == bully)
+          //not a bully message
+          else 
           {
-            if(users[i].feedAction[k].replyTime[0])
+            //Victim stats
+            if (users[i].feedAction[k].post.actor.id == victim)
             {
-              mlm.BullyNoBullyReplies++;
+              if(users[i].feedAction[k].replyTime[0])
+              {
+                mlm.VictimNoBullyReplies++;
+              }
+
+              if(users[i].feedAction[k].liked)
+              {
+                mlm.VictimNoBullyLikes++;
+              }
             }
 
+            //bully stats
+            if (users[i].feedAction[k].post.actor.id == bully)
+            {
+              if(users[i].feedAction[k].replyTime[0])
+              {
+                mlm.BullyNoBullyReplies++;
+              }
+
+              if(users[i].feedAction[k].liked)
+              {
+                mlm.BullyNoBullyLikes++;
+              }
+            }
+
+            //total number of likes
             if(users[i].feedAction[k].liked)
             {
-              mlm.BullyNoBullyLikes++;
+              mlm.GeneralLikeNumber++;
             }
-          }
 
-          //total number of likes
-          if(users[i].feedAction[k].liked)
-          {
-            mlm.GeneralLikeNumber++;
-          }
+            //total number of flags
+            if(users[i].feedAction[k].flagTime[0])
+            {
+              mlm.GeneralFlagNumber++;
+            }
 
-          //total number of flags
-          if(users[i].feedAction[k].flagTime[0])
-          {
-            mlm.GeneralFlagNumber++;
-          }
-
-          //total read times, and average of all reads
-          if(users[i].feedAction[k].readTime[0])
-          {
-            mlm.TotalNumberRead++;
-            mlm.AveReadTime += users[i].feedAction[k].readTime.sum() / users[i].feedAction[k].readTime.length;
-            
+            //total read times, and average of all reads
+            if(users[i].feedAction[k].readTime[0])
+            {
+              mlm.TotalNonBullyPostRead++;
+              mlm.AveReadTime += users[i].feedAction[k].readTime.sum() / users[i].feedAction[k].readTime.length;
+              
+            }
           }
 
 
@@ -166,11 +287,11 @@ User.find()
 
         //get totalAverage
         mlm.AveReadTime = mlm.AveReadTime/mlm.TotalNumberRead;
+        mlm.TotalNumberRead = mlm.TotalNonBullyPostRead + bullyReads;
+        mlm.GeneralReplyNumber = users[i].numReplies + 1 - bullyReplies;
+        mlm.GeneralPostNumber = users[i].numPosts + 1;
 
-        mlm.GeneralReplyNumber = users[i].numReplies +1;
-        mlm.GeneralPostNumber = users[i].numPosts +1;
-
-        for (var n = bully_messages.length - 1; n >= 0; n--) 
+        for (var n = 0; n < bully_messages.length; n++) 
         {  
 
 
@@ -236,6 +357,7 @@ User.find()
           else
           {
             mlm.BullyingPost  = n + 1;
+            
             mlm.ReadTime = 0;
             mlm.AverageReadTime = 0;
             mlm.ReadTimes = 0;
@@ -245,6 +367,7 @@ User.find()
             mlm.LikeTime = 0;
             mlm.Reply = 0;
             mlm.ReplyTime = 0;
+            
           }
 
           mlm_writer.write(mlm);
@@ -280,14 +403,16 @@ User.find()
         sur.ReportHours = 259200000;
         sur.reportIssue = "";
       }
+
       sur.VictimNoBullyReplies = mlm.VictimNoBullyReplies;
       sur.VictimNoBullyLikes = mlm.VictimNoBullyLikes;
-      sur.BullyNoBullyReplies = mlm.BullyNoBullyReplies;
-      sur.BullyNoBullyLikes = mlm.BullyNoBullyLikes;
-      sur.GeneralLikeNumber = mlm.GeneralLikeNumber;
-      sur.GeneralFlagNumber = mlm.GeneralFlagNumber;
-      sur.GeneralReplyNumber = mlm.GeneralReplyNumber;
+      sur.BullyNoBullyReplies = mlm.BullyNoBullyReplies + bullyReplies;
+      sur.BullyNoBullyLikes = mlm.BullyNoBullyLikes + bullyLikes;
+      sur.GeneralLikeNumber = mlm.GeneralLikeNumber + bullyLikes;
+      sur.GeneralFlagNumber = mlm.GeneralFlagNumber + bullyFlag;
+      sur.GeneralReplyNumber = mlm.GeneralReplyNumber + bullyReplies;
       sur.GeneralPostNumber = mlm.GeneralPostNumber;
+      sur.TotalNumberRead = mlm.TotalNumberRead;
       s_writer.write(sur);
     }//for each user
 
