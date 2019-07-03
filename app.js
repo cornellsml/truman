5d22aedab3fb0beccf4060b2032b7b662a881c73
Dominic Difranzo <dominic@difranzo.com>
2019-07-03 14:05:28 -0400

    delete SASS mod

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
-const sass = require('node-sass-middleware');
 var schedule = require('node-schedule');
 
 const multer = require('multer');
@@ -160,10 +159,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 //app.use(expressStatusMonitor());
 //app.use(compression());
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
